b1b7d1e5fc353a340dea9d3e191e5e004804052e
Radun111 <thesanyaraduni@gmail.com>
2025-02-20 21:11:57 +0530

    Create Login Form UI for Course Registration System

---
+++ src/Main.java
@@ -0,0 +1,27 @@
+import java.net.URL;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args)  {
+        System.out.println("Testing GitHub Connection!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        URL resource = getClass().getResource("/view/Login.fxml"); 
+        if (resource == null) {
+            throw new IllegalStateException("FXML file 'Main.fxml' not found in /view directory!");
+        }
+        Parent root = FXMLLoader.load(resource);
+        primaryStage.setScene(new Scene(root));
+        primaryStage.setTitle("Login");
+        primaryStage.show();
+    }
+
+}
