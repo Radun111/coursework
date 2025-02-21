ca36e1f6aa8cac1d2788919e49a16270b943f097
Radun111 <thesanyaraduni@gmail.com>
2025-02-21 10:29:33 +0530

    Create Student View Courses UI

--- src/Main.java
+++ src/Main.java
@@ -14,13 +14,13 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        URL resource = getClass().getResource("/view/StuProfile.fxml"); 
+        URL resource = getClass().getResource("/view/StuViewCourses.fxml"); 
         if (resource == null) {
             throw new IllegalStateException("FXML file 'Main.fxml' not found in /view directory!");
         }
         Parent root = FXMLLoader.load(resource);
         primaryStage.setScene(new Scene(root));
-        primaryStage.setTitle("StuProfile");
+        primaryStage.setTitle("Student View Courses");
         primaryStage.show();
     }
 
