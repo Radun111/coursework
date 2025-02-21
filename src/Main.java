a7999f3dcfc32b39a78d9c33139b06627a483f50
Radun111 <thesanyaraduni@gmail.com>
2025-02-21 19:46:47 +0530

    Create Admin Academic Records UI and Reports Analytics

--- src/Main.java
+++ src/Main.java
@@ -14,7 +14,7 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        URL resource = getClass().getResource("/view/AdminEnrollmentManagement.fxml"); 
+        URL resource = getClass().getResource("/view/AdminReportsAnalytics.fxml"); 
         if (resource == null) {
             throw new IllegalStateException("FXML file 'Main.fxml' not found in /view directory!");
         }
